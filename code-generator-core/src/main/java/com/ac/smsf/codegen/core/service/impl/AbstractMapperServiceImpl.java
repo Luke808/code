941b5d5032c9764373fbc78bad9b546023e9c0b7
s.c.gao <s.c.gao@accenture.com>
2018-12-11 14:45:32 +0800

    支持条件查询

--- code-generator-core/src/main/java/com/ac/smsf/codegen/core/service/impl/AbstractMapperServiceImpl.java
+++ code-generator-core/src/main/java/com/ac/smsf/codegen/core/service/impl/AbstractMapperServiceImpl.java
@@ -3,15 +3,21 @@ package com.ac.smsf.codegen.core.service.impl;
 import com.ac.smsf.codegen.core.mapper.BaseMapper;
 import com.ac.smsf.codegen.core.service.MapperService;
 import com.github.pagehelper.PageHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import tk.mybatis.mapper.entity.Condition;
+import tk.mybatis.mapper.entity.Example;
 
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 /**
- * @author s.c.gao
  * @param <T>
+ * @author s.c.gao
  */
+@Slf4j
 public abstract class AbstractMapperServiceImpl<T> implements MapperService<T> {
 
     @Autowired
@@ -56,7 +62,34 @@ public abstract class AbstractMapperServiceImpl<T> implements MapperService<T> {
     @Override
     public List<T> findBy(T t, int pageNumber, int pageSize) {
         Condition condition = new Condition(t.getClass(), false, false);
+        Example.Criteria criteria = condition.createCriteria();
+        Field[] fields = t.getClass().getDeclaredFields();
+        try {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                Object value = field.get(t);
+                if (value == null) {
+                    continue;
+                }
+                Class<?> type = field.getType();
+                if (type == int.class || type == Integer.class || type == long.class ||
+                        type == Long.class || type == double.class || type == Double.class ||
+                        type == float.class || type == Float.class || type == Date.class) {
+                    // 数字和日期类型 == 匹配
+                    criteria.andEqualTo(field.getName(), value);
+                } else if (type == String.class) {
+                    // 字符类型模糊匹配，为了使用索引，只支持后置模糊匹配
+                    criteria.andLike(field.getName(), value + "%");
+                } else {
+                    // 其他情况暂不处理
+                }
+            }
+        } catch (Exception e) {
+            log.error("查询参数解析异常", e);
+            return Collections.emptyList();
+        }
         PageHelper.startPage(pageNumber, pageSize);
         return baseMapper.selectByCondition(condition);
     }
+
 }
